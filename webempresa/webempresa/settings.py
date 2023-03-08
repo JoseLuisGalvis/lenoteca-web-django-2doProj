3dc4848bab7762700f1e95fc06493d13c4176e26
JoseLuisGalvis <jolugales@gmail.com>
2023-03-08 17:47:53 -0300

    pswd

--- webempresa/webempresa/settings.py
+++ webempresa/webempresa/settings.py
@@ -128,7 +128,7 @@ EMAIL_HOST = 'smtp.gmail.com'
 EMAIL_USE_TLS = True
 EMAIL_PORT = 587
 EMAIL_HOST_USER = 'jolugales@gmail.com'
-EMAIL_HOST_PASSWORD = 'yrsa6662'
+EMAIL_HOST_PASSWORD = ''
 
 import os
 
